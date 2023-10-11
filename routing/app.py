49f6f58790ca757ffd32d7820a013882d5af2244
Apple1704 <vyshnavimanthena7@gmail.com>
2023-10-11 21:00:14 +0530

    htmltopdf

--- routing/app.py
+++ routing/app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 from pdftodocx import pdftodocx_blueprint
 from doctopdf import doctopdf_blueprint
 from doctoimg import doctoimg_blueprint
-from docxtopptx import docxtopptx_blueprint
+#from docxtopptx import docxtopptx_blueprint
 from pdftoimg import pdf2img_bp
-from pdftoppt import pdftoppt_bp
-from ppttopdf import ppttopdf_bp
-from ppttoimg import ppttoimages_bp
+# from pdftoppt import pdftoppt_bp
+# from ppttopdf import ppttopdf_bp
+# from ppttoimg import ppttoimages_bp
 from imgtopdf import imgtopdf_bp
 
 app = Flask(__name__)
@@ -14,11 +14,11 @@ app = Flask(__name__)
 app.register_blueprint(pdftodocx_blueprint)
 app.register_blueprint(doctopdf_blueprint)
 app.register_blueprint(doctoimg_blueprint)
-app.register_blueprint(docxtopptx_blueprint)
+# app.register_blueprint(docxtopptx_blueprint)
 app.register_blueprint(pdf2img_bp)
-app.register_blueprint(pdftoppt_bp) 
-app.register_blueprint(ppttopdf_bp)
-app.register_blueprint(ppttoimages_bp)
+# app.register_blueprint(pdftoppt_bp) 
+# app.register_blueprint(ppttopdf_bp)
+# app.register_blueprint(ppttoimages_bp)
 app.register_blueprint(imgtopdf_bp)
 
 app.secret_key = '2208'
