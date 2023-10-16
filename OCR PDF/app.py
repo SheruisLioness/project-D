d6cb8cab0bf12bb0722a249eddd66a8fd8b31182
SheruisLioness <bgk09112005@gmail.com>
2023-10-16 13:55:40 +0530

    Revert "OCR PDF DEL"
    This reverts commit 12035951cf0051e2889de2c41863592ba7da5594.

---
+++ OCR PDF/app.py
@@ -0,0 +1,28 @@
+from flask import Flask , render_template
+import os
+from ocr import ocr_blueprint
+
+app = Flask(__name__)
+app.secret_key = '0911'
+
+UPLOAD_FOLDER = 'uploads'
+DOWNLOAD_FOLDER = 'downloads'
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+if not os.path.exists(DOWNLOAD_FOLDER):
+    os.makedirs(DOWNLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+
+# Register the blueprint with a URL prefix (e.g., '/ocr')
+app.register_blueprint(ocr_blueprint, url_prefix='/ocr')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
