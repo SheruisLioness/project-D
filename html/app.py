60da74ae8cb170c67f535820afa777a5e702a67e
Apple1704 <141623341+Apple1704@users.noreply.github.com>
2023-11-03 13:28:01 +0530

    html

--- html/app.py
+++ html/app.py
@@ -1,34 +1,10 @@
-from flask import Flask, render_template, request, send_file
-import pdfkit
-import os
+from flask import Flask
+from htmltopdf.htmltopdf_blueprint import htmltopdf_bp  # Import the blueprint
 
 app = Flask(__name__)
 
-# Set the path to wkhtmltopdf executable
-wkhtmltopdf_path = r'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'  # Replace with your actual path
-config = pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/convert', methods=['POST'])
-def convert():
-    html_file = request.files['htmlFile']
-
-    # Save the uploaded HTML file to a temporary file
-    temp_html_path = 'temp.html'
-    html_file.save(temp_html_path)
-
-    # Convert HTML to PDF using pdfkit
-    output_pdf_path = 'output.pdf'
-    pdfkit.from_file(temp_html_path, output_pdf_path, configuration=config)
-
-    # Delete the temporary HTML file
-    os.remove(temp_html_path)
-
-    # Send the generated PDF as a response
-    return send_file(output_pdf_path, as_attachment=True)
+# Register the blueprint
+app.register_blueprint(htmltopdf_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ html/htmltopdf/htmltopdf_blueprint.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template, request, send_file
+import pdfkit
+import os
+
+htmltopdf_bp = Blueprint('htmltopdf', __name__)
+
+# Set the path to wkhtmltopdf executable
+wkhtmltopdf_path = r'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe'
+config = pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path)
+
+@htmltopdf_bp.route('/')
+def index():
+    return render_template('htmltopdf.html')
+
+@htmltopdf_bp.route('/convert', methods=['POST'])
+def convert():
+    html_file = request.files['htmlFile']
+
+    # Save the uploaded HTML file to a temporary file
+    temp_html_path = 'temp.html'
+    html_file.save(temp_html_path)
+
+    # Convert HTML to PDF using pdfkit
+    output_pdf_path = 'output.pdf'
+    pdfkit.from_file(temp_html_path, output_pdf_path, configuration=config)
+
+    # Delete the temporary HTML file
+    os.remove(temp_html_path)
+
+    # Send the generated PDF as a response
+    return send_file(output_pdf_path, as_attachment=True)
